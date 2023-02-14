8c2c4040e95ff067ba99882bac23bd20d0d7ec0e
Lucas Pedro Abreu <lucasp.abreu@gmail.com>
2023-02-14 16:43:05 -0300

    chore: prepare config for production

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,9 +11,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_CLIENT: z.string().default('sqlite3'),
+  DATABASE_CLIENT: z.enum(['sqlite3', 'pg', 'postgres']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 // zod parses process.env against our schema
